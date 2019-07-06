1f2b0686e403fc4154c240528c451f1ba213e68d
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-07-06 15:19:31 +0200

    add properties && annotation eureka

--- src/main/java/org/Kader/ServiceRegisterEurekaApplication.java
+++ src/main/java/org/Kader/ServiceRegisterEurekaApplication.java
@@ -2,7 +2,8 @@ package org.Kader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegisterEurekaApplication {
 

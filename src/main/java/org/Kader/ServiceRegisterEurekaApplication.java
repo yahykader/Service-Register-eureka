9e1aa45991250e8f0725dd75ed1fa6cc0eb0380c
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-07-06 17:01:25 +0200

    add eurekaregister

--- src/main/java/org/Kader/ServiceRegisterEurekaApplication.java
+++ src/main/java/org/Kader/ServiceRegisterEurekaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceRegisterEurekaApplication {
 
